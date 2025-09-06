de2800d5a276c65f8bc03d5e7343b21b04c3d928
camicarrizouade <camicarrizo@uade.edu.ar>
2025-09-06 10:33:36 -0300

    Agregue validaciones para el inquilino

---
+++ FuncAux/validaciones.py
@@ -0,0 +1,52 @@
+import re
+
+def validar_id(valor):
+    texto = str(valor).strip()
+    coincide = re.match(r"^[0-9]+$", texto)
+    if coincide is None:
+        return False
+    else:
+        return True
+
+def validar_nombre(valor):
+    texto = str(valor).strip()
+    coincide = re.match(r"^[A-Za-zÁÉÍÓÚáéíóúÑñ ]{2,60}$", texto)
+    if coincide is None:
+        return False
+    else:
+        return True
+
+def validar_dni(valor):
+    texto = str(valor).strip()
+    coincide = re.match(r"^[0-9]{7,8}$", texto)
+    if coincide is None:
+        return False
+    else:
+        return True
+
+
+def validar_telefono(valor):
+    texto = str(valor)
+    solo_dig = re.findall(r"[0-9]", texto)
+    cantidad = len(solo_dig)
+    if cantidad >= 6 and cantidad <= 15:
+        return True
+    else:
+        return False
+
+def validar_inquilino(fila, encabezados):               #para verificar si los inquilinos están bien subidos
+    i_id  = encabezados.index("ID Inquilino")
+    i_nom = encabezados.index("Nombre")
+    i_dni = encabezados.index("DNI")
+    i_tel = encabezados.index("Teléfono")
+
+    if not validar_id(fila[i_id]):
+        return False
+    if not validar_nombre(fila[i_nom]):
+        return False
+    if not validar_dni(fila[i_dni]):
+        return False
+    if not validar_telefono(fila[i_tel]):
+        return False
+
+    return True
