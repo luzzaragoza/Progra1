f62d40dea15c0fe3f8fe53727b0d14e2eecefded
Luz <mazaragoza@uade.edu.ar>
2025-09-14 23:10:19 -0300

    Module funcion crearcontratos

--- Contratos/crear.py
+++ Contratos/crear.py
@@ -1,3 +1,14 @@
+def crear_contrato(id_propiedad):
+    id_propiedad = input("ID de la propiedad: ").strip()
+    id_inquilino = input("ID del inquilino: ").strip()
+    fecha_inicio = input("Fecha de inicio (YYYY-MM-DD): ").strip()
+    fecha_fin    = input("Fecha de fin (YYYY-MM-DD): ").strip()
+    monto_mensual= input("Monto mensual: ").strip()
+    
+    return [id_propiedad, id_inquilino, fecha_inicio, fecha_fin, monto_mensual, True]
+
+
+
 def crear_matriz_contrato(cant_contratos):
     """
     Crea una matriz con contratos según la cantidad pedida.
@@ -9,14 +20,7 @@ def crear_matriz_contrato(cant_contratos):
 
     for i in range(cant_contratos):
         ID_contrato = len(contratos) + 1  
-
-        id_propiedad = input("ID de la propiedad: ").strip()
-        id_inquilino = input("ID del inquilino: ").strip()
-        fecha_inicio = input("Fecha de inicio (YYYY-MM-DD): ").strip()
-        fecha_fin    = input("Fecha de fin (YYYY-MM-DD): ").strip()
-        monto_mensual= input("Monto mensual: ").strip()
-
-        contrato = [ID_contrato, id_propiedad, id_inquilino, fecha_inicio, fecha_fin, monto_mensual, True]
+        contrato = crear_contrato(ID_contrato)
 
         contratos.append(contrato)
 
